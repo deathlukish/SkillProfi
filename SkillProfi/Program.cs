0c805812606426f122194e1b8b33b0bbc7cb2338
Дмитрий <deathlukish@mail.ru>
2023-06-23 15:46:09 +0300

    подготовка инфраструктуры

--- SkillProfi/Program.cs
+++ SkillProfi/Program.cs
@@ -3,7 +3,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddAuthentication(opts =>
+{
+    opts.DefaultScheme =
+        JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opts =>
+{
+    opts.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = "Server",
+        ValidateAudience = true,
+        ValidAudience = "Client",
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superpupersecurity")),
+        ValidateIssuerSigningKey = true,
+    };
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
