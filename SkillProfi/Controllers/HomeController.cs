0bb3edd6769a62109f407299c0bdb8a40811ac7f
Дмитрий <deathlukish@mail.ru>
2023-06-23 15:23:51 +0300

    подготовка инфраструктуры API

---
+++ SkillProfi/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SkillProfiApi.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+       
+    }
+}
--- SkillProfi/Program.cs
+++ SkillProfi/Program.cs
@@ -10,6 +10,7 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
